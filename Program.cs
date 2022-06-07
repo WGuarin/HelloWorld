e78fc33d3dd6df1e64873f8719edff23d7bad1be
William <wguarino@ucentral.edu.co>
2022-06-07 12:17:49 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+public class Vehicle{
+    private int llantas;
+
+    public Vehicle(int llantas){
+        this.llantas = llantas;
+    }
+    public int getLlantas(){
+        return this.llantas;
+    }
+    public void setLlantas(int llantas){
+        this.llantas = llantas;
+    }
+}
+public class Car<TPaint, TPlaca> : Vehicle{
+    private TPaint paintColor;
+    private TPlaca placa;
+    private int cambio;
+
+    public Car(int llantas, TPaint color, TPlaca placa) : base(llantas){
+        this.paintColor = color;
+        this.placa = placa;
+        this.cambio = 0;
+    }
+
+    public String subirCambio(){
+        cambio++;
+        return "Cambio carro: " + cambio;
+    }
+    public String bajarCambio(){
+        cambio--;
+        return "Cambio carro: " + cambio;
+    }
+
+
+    public TPaint getPaintColor(){
+        return this.paintColor;;
+    }
+    public void setPaintColor(TPaint paintColor){
+        this.paintColor = paintColor;
+    }
+    public TPlaca getPlaca(){
+        return this.placa;
+    }
+    public void setPlaca(TPlaca placa){
+        this.placa = placa;
+    }
+
+    public bool Equals(Car<TPlaca, TPaint> a){
+        return string.Equals(a.getPlaca(), this.getPlaca());
+    }
+
+}
+public sealed class Moto : Vehicle{
+    private int patas, cambio;
+
+    public Moto(int llantas, int patas) : base(llantas){
+        this.patas = patas;
+        this.cambio = 0;
+    }
+
+    public String subirCambio(){
+        cambio++;
+        return "Cambio moto: " + cambio;
+    }
+    public String bajarCambio(){
+        cambio--;
+        
+        return "Cambio moto: " + cambio;
+    }
+}
+public class Program{
+    static void Main(String[] args){
+    
+        var settings = MongoClientSettings.FromConnectionString("mongodb+srv://wguarino:a19369099@cluster0.zdpts.mongodb.net/?retryWrites=true&w=majority");
+        var client = new MongoClient(settings);
+        var database = client.GetDatabase("myFirstDatabase");
+        var collection = database.GetCollection<BsonDocument>("collection1");
+        
+        //var json = JsonConvert.DeserializeObject();
+        var res = collection.Aggregate().Group(new BsonDocument{
+            {"_id", "$League" },
+            {"count", new BsonDocument("$sum", 1)}
+        }).Match(new BsonDocument{
+            {"_id", "EU"}
+        });    
+        foreach (var item in res.ToEnumerable()){
+            Console.WriteLine(item);
+        }
+    }
+}
